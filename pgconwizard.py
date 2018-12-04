89f4d7b7e7b716feb554b6b58528573abd6f18d4
Sergey <xxsmotur@gmail.com>
2018-12-05 02:00:53 +0500

    init for the main class

--- pgconwizard.py
+++ pgconwizard.py
@@ -6,3 +6,15 @@ def total_memory():
     on local machine
     '''
     return virtual_memory().total
+
+
+class PgConfWizard:
+    def __init__(self, path):
+        self.path = path
+        self.config = {}
+    
+    def read_file(self):
+        for i, line in enumerate(open(self.filename)):
+            splitter = line.split('\n')
+            self.config[splitter[0] = splitter[1]
+
